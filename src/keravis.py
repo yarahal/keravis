be4e708781092466fdab85bb94dc5a8a2e0f7861
yarahal <yarahalawi12@gmail.com>
2021-09-05 21:06:53 +0300

    Many updates

--- src/keravis.py
+++ src/keravis.py
@@ -1,24 +1,26 @@
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.manifold import TSNE
 from sklearn.decomposition import PCA
-from utils import find_closest_factors
-from math import sqrt, log
+from utils import pixel_scaling, find_closest_factors, clone_function_1, guided_relu, guided_linear, clone_function_2
+from math import log
+
 
 def conv_layer_activations(model,
                            layer,
                            test_img,
+                           nested_model=None,
                            title=None):
     '''
-    Visualizes activations of a given convolutional layer for a given image
+    Visualizes activations of `layer` corresponding to `test_img` in a grid
 
     Parameters
     ----------
-    model : keras Model
-    layer : keras Layer
-        Layer
+    model : keras.Model
+    layer : str
+        Layer name
+    nested_model : str
     test_img : ndarray
         Image for which to look at activations of
 
@@ -27,27 +29,43 @@ def conv_layer_activations(model,
     A grid of activations of the given layer corresponding to the test image
     '''
 
-    # create model whose output is output of conv_layer
-    modified_model = tf.keras.Model(inputs=model.input,outputs=layer.output)
-    
+    # create modified_model whose output is output of conv_layer
+    if nested_model is None:
+        conv_layer = model.get_layer(layer)
+        modified_model = tf.keras.Model(
+            inputs=nested_model.input, outputs=conv_layer.output)
+    else:
+        nested_model = model.get_layer(nested_model)
+        conv_layer = nested_model.get_layer(layer)
+        x = model.input
+        for layer in model.layers:
+            if layer == nested_model:
+                break
+            x = layer(x)
+        x = tf.keras.Model(nested_model.input, conv_layer.output)(x)
+        modified_model = tf.keras.Model(inputs=model.input, outputs=x)
+
     # retrieve activations of test_img
-    activations = modified_model(np.expand_dims(test_img,0))
+    activations = modified_model(np.expand_dims(test_img, 0))
     n_channels = activations.shape[3]
-    
-    # create grid and plot filters
+
+    # create grid and plot activations
     rows, cols = find_closest_factors(n_channels)
-    n_pixels = activations.shape[1]
-    fig, axs = plt.subplots(rows,cols,figsize=(0.02*n_pixels*cols,0.02*n_pixels*rows),gridspec_kw={'wspace':0.1, 'hspace':0.1})
+    width, height = pixel_scaling(
+        activations.shape[1])*cols, pixel_scaling(activations.shape[2])*rows
+    fig, axs = plt.subplots(rows, cols, figsize=(
+        width, height), gridspec_kw={'wspace': 0.1, 'hspace': 0.1})
     if title is not None:
         fig.suptitle(title)
     axs = axs.flatten()
     for i in range(n_channels):
         ax = axs[i]
-        activation = activations[0,:,:,i]
-        ax.imshow(activation,cmap='gray')
+        activation = activations[0, :, :, i]
+        ax.imshow(activation, cmap='gray')
         ax.grid(False)
         ax.set_xticks([])
-        ax.set_yticks([]) 
+        ax.set_yticks([])
+
 
 def feature_space_tsne(model,
                        dataset=None,
@@ -55,8 +73,9 @@ def feature_space_tsne(model,
                        y=None,
                        title=None):
     '''
-    Visualizes activations of the last fully-connected layer on a set of images in 2-dimensional space using tSNE
-        
+    Visualizes activations of the last fully-connected layer before the output layer
+    on a set of images in 2-dimensional space using tSNE
+
     Parameters
     ----------
     model : keras Model
@@ -74,32 +93,35 @@ def feature_space_tsne(model,
     '''
     # find the fc layer
     fc_layer = model.layers[-2]
-    
+
     # create model whose output is output of fc_layer
-    modified_model = tf.keras.Model(inputs=model.input,outputs=fc_layer.output)
+    modified_model = tf.keras.Model(
+        inputs=model.input, outputs=fc_layer.output)
 
     # set X and y to a batch of images and labels if dataset is given
     if dataset is not None:
         X, y = dataset.next()
 
-    # store fc layer for images in X
+    # store fc_layer activations for images in X
     fc_layer_activations = []
     for img in X:
-        fc_layer_activations.append(np.array(modified_model(np.expand_dims(img,0))).flatten())
+        fc_layer_activations.append(
+            np.array(modified_model(np.expand_dims(img, 0))).flatten())
     fc_layer_activations = np.array(fc_layer_activations)
-    
-    # fit and transform TSNE on X
+
+    # fit and transform TSNE on fc_layer_activations
     tsne = TSNE(n_components=2)
     transformed_X = tsne.fit_transform(fc_layer_activations)
-    
+
     # plot result
-    fig, ax = plt.subplots(figsize=(7,7))
-    scatter = ax.scatter(transformed_X[:,0],transformed_X[:,1],c=y)
+    fig, ax = plt.subplots(figsize=(7, 7))
+    scatter = ax.scatter(transformed_X[:, 0], transformed_X[:, 1], c=y)
     ax.set_xlabel('component 1')
     ax.set_ylabel('component 2')
     ax.legend(*scatter.legend_elements())
     if title is not None:
         ax.set_title(title)
+
 
 def feature_space_pca(model,
                       dataset=None,
@@ -107,8 +129,9 @@ def feature_space_pca(model,
                       y=None,
                       title=None):
     '''
-    Visualizes activations of the last fully-connected layer on a set of images in 2-dimensional space using PCA
-    
+    Visualizes activations of the last fully-connected layer before the output layer
+    on a set of images in 2-dimensional space using PCA
+
     Parameters
     ----------
     model : keras Model
@@ -126,32 +149,35 @@ def feature_space_pca(model,
     '''
     # find the fc layer
     fc_layer = model.layers[-2]
-    
+
     # create model whose output is output of fc_layer
-    modified_model = tf.keras.Model(inputs=model.input,outputs=fc_layer.output)
+    modified_model = tf.keras.Model(
+        inputs=model.input, outputs=fc_layer.output)
 
     # set X and y to a batch of images and labels if dataset is given
     if dataset is not None:
         X, y = dataset.next()
 
-    # store fc layer for images in X
+    # store fc layer activations for images in X
     fc_layer_activations = []
     for img in X:
-        fc_layer_activations.append(np.array(modified_model(np.expand_dims(img,0))).flatten())
+        fc_layer_activations.append(
+            np.array(modified_model(np.expand_dims(img, 0))).flatten())
     fc_layer_activations = np.array(fc_layer_activations)
-    
-    # fit and transform PCA on X
+
+    # fit and transform PCA on fc_layer_activations
     pca = PCA(n_components=2)
     transformed_X = pca.fit_transform(fc_layer_activations)
-    
+
     # plot result
-    fig, ax = plt.subplots(figsize=(7,7))
-    scatter = ax.scatter(transformed_X[:,0],transformed_X[:,1],c=y)
+    fig, ax = plt.subplots(figsize=(7, 7))
+    scatter = ax.scatter(transformed_X[:, 0], transformed_X[:, 1], c=y)
     ax.set_xlabel('component 1')
     ax.set_ylabel('component 2')
     ax.legend(*scatter.legend_elements())
     if title is not None:
         ax.set_title(title)
+
 
 def saliency_occlusion(model,
                        test_img,
@@ -159,7 +185,7 @@ def saliency_occlusion(model,
                        title=None):
     '''
     Visualizes the saliency map of an image using occulsion
-    
+
     Parameters
     ----------
     model : keras Model
@@ -172,39 +198,46 @@ def saliency_occlusion(model,
     -------
     Saliency map of test_img by occlusion
     '''
-    # get needed saliency map dimensions
+    # find saliency map dimensions
     mask_width, mask_height = test_img.shape[0]//4, test_img.shape[1]//4
     mask_stride = 1
-    width, height = (test_img.shape[0]-mask_width)//mask_stride, (test_img.shape[1]-mask_height)//mask_stride
-    
+    width, height = (
+        test_img.shape[0]-mask_width)//mask_stride, (test_img.shape[1]-mask_height)//mask_stride
+
     # create saliency map
-    saliency_map = np.zeros((width,height))
-    
-    # fill saliency map by moving mask across image
-    for i in range(0,width,mask_stride):
-        for j in range(0,height,mask_stride):
+    saliency_map = np.zeros((width, height))
+
+    # fill saliency map by sliding mask across image
+    for i in range(0, width, mask_stride):
+        for j in range(0, height, mask_stride):
             img_clipped = np.copy(test_img)
-            img_clipped[i:i+mask_width,j:j+mask_height,:] = 0
-            saliency_map[i,j] = np.array(model(np.expand_dims(img_clipped,0))).flatten()[class_idx]
-    
+            img_clipped[i:i+mask_width, j:j+mask_height, :] = 0
+            saliency_map[i, j] = np.array(
+                model(np.expand_dims(img_clipped, 0))).flatten()[class_idx]
+
     # plot result
-    n_pixels_img = test_img.shape[0]
-    fig, (ax1, ax2) = plt.subplots(1,2,figsize=(0.02*n_pixels_img*2,0.02*n_pixels_img),gridspec_kw={'wspace':0, 'hspace':0})
+    width, height = pixel_scaling(
+        test_img.shape[0])*2, pixel_scaling(test_img.shape[1])
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(width,
+                                                  height), gridspec_kw={'wspace': 0, 'hspace': 0})
     if title is not None:
         fig.suptitle(title)
-    ax1.imshow(test_img,aspect='auto')
+    ax1.imshow(test_img, aspect='auto')
     ax1.grid(False)
     ax1.set_xticks([])
     ax1.set_yticks([])
     ax1.set_title("test image")
-    ax2.imshow(saliency_map,aspect='auto',cmap='jet',interpolation='bilinear')
+    ax2.imshow(saliency_map, aspect='auto',
+               cmap='jet', interpolation='bilinear')
     ax2.grid(False)
     ax2.set_xticks([])
     ax2.set_yticks([])
     ax2.set_title("saliency map")
 
+
 def saliency_backprop(model,
                       test_img,
+                      class_idx=0,
                       title=None):
     '''
     Visualizes the saliency map of an image using backprop
@@ -220,48 +253,102 @@ def saliency_backprop(model,
     Saliency map of test_img by backprop
     '''
 
-    # create layer to invert softmax activation
-    invert_layer = tf.keras.layers.Lambda(lambda x : tf.math.log(x)+ log(10.))
-    
-    # create model whose output is output of invert_layer
-    x = model(model.input)
-    x = invert_layer(x)
-    modified_model = tf.keras.Model(inputs=model.input,outputs=x)
-    
+    # create modified_model with softmax activation of output layer removed to get raw class scores
+    modified_model = tf.keras.models.clone_model(
+        model, model.input, clone_function_1)
+    modified_model.set_weights(model.get_weights())
+
     # convert image to tensor
-    tensor_img = tf.convert_to_tensor(np.expand_dims(test_img,0))
-    
-    # compute gradient of invert_layer wrt to input image 
+    tensor_img = tf.convert_to_tensor(np.expand_dims(test_img, 0))
+
+    # compute gradient of output wrt to input image
     with tf.GradientTape() as tape:
         tape.watch(tensor_img)
-        output = modified_model(tensor_img)
-    gradients = tape.gradient(output,tensor_img)
-    saliency_map = np.max(np.abs(gradients[0,:,:,:]),axis=2)
+        output = modified_model(tensor_img)[:, class_idx]
+    gradients = tape.gradient(output, tensor_img)
+    saliency_map = np.max(np.abs(gradients[0, :, :, :]), axis=2)
 
     # plot result
-    n_pixels = test_img.shape[0]
-    fig, (ax1, ax2) = plt.subplots(1,2,figsize=(0.02*n_pixels*2,0.02*n_pixels),gridspec_kw={'wspace':0, 'hspace':0})
+    width, height = pixel_scaling(
+        test_img.shape[0])*2, pixel_scaling(test_img.shape[1])
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(width,
+                                                  height), gridspec_kw={'wspace': 0, 'hspace': 0})
     if title is not None:
         fig.suptitle(title)
-    ax1.imshow(test_img,aspect='auto')
+    ax1.imshow(test_img, aspect='auto')
     ax1.grid(False)
     ax1.set_xticks([])
     ax1.set_yticks([])
     ax1.set_title("test image")
-    ax2.imshow(saliency_map,aspect='auto',cmap='jet',interpolation='bilinear')
+    ax2.imshow(saliency_map, aspect='auto',
+               cmap='jet', interpolation='bilinear')
     ax2.grid(False)
     ax2.set_xticks([])
     ax2.set_yticks([])
     ax2.set_title("saliency map")
 
-def conv_features_backprop(model,
-                          layer,
-                          test_img,
-                          channel=None,
-                          n_neurons=10,
-                          title=None):
+
+def saliency_guided_backprop(model,
+                             test_img,
+                             class_idx,
+                             title=None):
     '''
-    Visualizes gradients of intermediate neurons in the given layer corresponding to a test image using backprop
+    Visualizes the saliency map of an image using guided backprop
+
+    Parameters
+    ----------
+    model : keras Model
+    test_img : ndarray
+        Image for which to find saliency map
+
+    Outputs
+    -------
+    Saliency map of test_img by guided backprop
+    '''
+
+    # create modified model for guided backprop
+    modified_model = tf.keras.models.clone_model(
+        model, model.input, clone_function_2)
+    modified_model.set_weights(model.get_weights())
+
+    # convert image to tensor
+    tensor_img = tf.convert_to_tensor(np.expand_dims(test_img, 0))
+
+    # compute gradient of output wrt to input image
+    with tf.GradientTape() as tape:
+        tape.watch(tensor_img)
+        output = modified_model(tensor_img)[:, class_idx]
+    gradients = tape.gradient(output, tensor_img)
+    saliency_map = np.max(np.abs(gradients[0, :, :, :]), axis=2)
+
+    # plot result
+    width, height = pixel_scaling(
+        test_img.shape[0])*2, pixel_scaling(test_img.shape[1])
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(width,
+                                                  height), gridspec_kw={'wspace': 0, 'hspace': 0})
+    if title is not None:
+        fig.suptitle(title)
+    ax1.imshow(test_img, aspect='auto')
+    ax1.grid(False)
+    ax1.set_xticks([])
+    ax1.set_yticks([])
+    ax1.set_title("test image")
+    ax2.imshow(saliency_map, aspect='auto',
+               cmap='jet', interpolation='bilinear')
+    ax2.grid(False)
+    ax2.set_xticks([])
+    ax2.set_yticks([])
+    ax2.set_title("saliency map")
+
+
+def classifier_gradient_ascent(model,
+                               layer,
+                               test_img,
+                               channel=None,
+                               n_neurons=10,
+                               title=None):
+    '''
+    Visualizes gradients of intermediate neurons in the given layer corresponding to a test image using guided backprop
 
     Parameters
     ----------
@@ -277,37 +364,96 @@ def conv_features_backprop(model,
     -------
     A grid of gradients of n_neurons random neurons in layer wrt to test_img
     '''
-    
+
     # create model whose output is output of layer
-    modified_model = tf.keras.Model(inputs=model.input,outputs=layer.output)
-    
+    modified_model = tf.keras.Model(inputs=model.input, outputs=layer.output)
+
     # convert image to tensor
-    tensor_img = tf.convert_to_tensor(np.expand_dims(test_img,0))
+    tensor_img = tf.convert_to_tensor(np.expand_dims(test_img, 0))
 
     # choose a random channel if not given and random neurons
     if channel is None:
-        channel = np.random.randint(0,layer.filters)
-    random_neuron_idxs = zip(np.random.randint(0,layer.output_shape[1],n_neurons),np.random.randint(0,layer.output_shape[2],n_neurons))
-    
-    # record forward prop of image 
-    with tf.GradientTape() as tape:
-        tape.watch(tensor_img)
-        output = modified_model(tensor_img)
+        channel = np.random.randint(0, layer.filters)
+    random_neuron_idxs = list(zip(np.random.randint(
+        0, layer.output_shape[1], n_neurons), np.random.randint(0, layer.output_shape[2], n_neurons)))
 
     # compute and plot gradients of neurons wrt pixels of input image
     rows, cols = find_closest_factors(n_neurons)
     n_pixels = test_img.shape[0]
-    fig, axs = plt.subplots(1,2,figsize=(0.02*n_pixels*cols,0.02*n_pixels*rows),gridspec_kw={'wspace':0, 'hspace':0})
+    fig, axs = plt.subplots(rows, cols, figsize=(
+        0.02*n_pixels*cols, 0.02*n_pixels*rows), gridspec_kw={'wspace': 0, 'hspace': 0})
     if title is not None:
         fig.suptitle(title)
     axs = axs.flatten()
-    image_idxs = image_idxs.flatten()
     for k in range(n_neurons):
         ax = axs[k]
-        i, j = random_neuron_idxs[0][k], random_neuron_idxs[1][k]
-        feature_gradients = tape.gradient(output[:,i,j,channel],tensor_img)
-        feature_gradients = np.max(np.abs(feature_gradients[0,:,:,:]),axis=2)
-        ax.imshow(feature_gradients,aspect='auto')
+        i, j = random_neuron_idxs[k][0], random_neuron_idxs[k][1]
+        with tf.GradientTape() as tape:
+            tape.watch(tensor_img)
+            output = modified_model(tensor_img)[:, i, j, channel]
+        feature_gradients = tape.gradient(output, tensor_img)
+        feature_gradients = np.max(
+            np.abs(feature_gradients[0, :, :, :]), axis=2)
+        ax.imshow(feature_gradients, aspect='auto')
+        ax.grid(False)
+        ax.set_xticks([])
+        ax.set_yticks([])
+
+
+def conv_features_gradient_ascent(model,
+                                  layer,
+                                  test_img,
+                                  channel=None,
+                                  n_neurons=10,
+                                  title=None):
+    '''
+    Visualizes gradients of intermediate neurons in the given layer corresponding to a test image using guided backprop
+
+    Parameters
+    ----------
+    model : keras Model
+    layer : keras Layer
+        Layer 
+    test_img : ndarray
+        Image for which to look at intermediate features
+    channel : int
+    n_neurons : int
+
+    Outputs
+    -------
+    A grid of gradients of n_neurons random neurons in layer wrt to test_img
+    '''
+
+    # create model whose output is output of layer
+    modified_model = tf.keras.Model(inputs=model.input, outputs=layer.output)
+
+    # convert image to tensor
+    tensor_img = tf.convert_to_tensor(np.expand_dims(test_img, 0))
+
+    # choose a random channel if not given and random neurons
+    if channel is None:
+        channel = np.random.randint(0, layer.filters)
+    random_neuron_idxs = list(zip(np.random.randint(
+        0, layer.output_shape[1], n_neurons), np.random.randint(0, layer.output_shape[2], n_neurons)))
+
+    # compute and plot gradients of neurons wrt pixels of input image
+    rows, cols = find_closest_factors(n_neurons)
+    n_pixels = test_img.shape[0]
+    fig, axs = plt.subplots(rows, cols, figsize=(
+        0.02*n_pixels*cols, 0.02*n_pixels*rows), gridspec_kw={'wspace': 0, 'hspace': 0})
+    if title is not None:
+        fig.suptitle(title)
+    axs = axs.flatten()
+    for k in range(n_neurons):
+        ax = axs[k]
+        i, j = random_neuron_idxs[k][0], random_neuron_idxs[k][1]
+        with tf.GradientTape() as tape:
+            tape.watch(tensor_img)
+            output = modified_model(tensor_img)[:, i, j, channel]
+        feature_gradients = tape.gradient(output, tensor_img)
+        feature_gradients = np.max(
+            np.abs(feature_gradients[0, :, :, :]), axis=2)
+        ax.imshow(feature_gradients, aspect='auto')
         ax.grid(False)
         ax.set_xticks([])
         ax.set_yticks([])
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,10 @@
-from math import sqrt
+from math import sqrt, log
+import tensorflow as tf
+
+
+def pixel_scaling(dim):
+    return log(dim)
+
 
 def find_closest_factors(n):
     '''
@@ -10,3 +16,42 @@ def find_closest_factors(n):
         x -= 1
         y = n/x
     return x, int(y)
+
+
+def clone_function_1(layer):
+    layer_config = layer.get_config()
+    if 'activation' in layer_config and (layer_config['activation'] == 'sigmoid' or layer_config['activation'] == 'softmax'):
+        layer_config['activation'] = 'linear'
+    new_layer = type(layer).from_config(layer_config)
+    return new_layer
+
+
+def clone_function_2(layer):
+    if isinstance(layer, tf.keras.Model):
+        return tf.keras.models.clone_model(layer, layer.input, clone_function)
+    layer_config = layer.get_config()
+    if 'activation' in layer_config and layer_config['activation'] == 'relu':
+        layer_config['activation'] = guided_relu
+    elif 'activation' in layer_config and (layer_config['activation'] == 'sigmoid' or layer_config['activation'] == 'softmax'):
+        layer_config['activation'] = guided_linear
+    new_layer = type(layer).from_config(layer_config)
+    return new_layer
+
+
+@tf.custom_gradient
+def guided_relu(x):
+    y = tf.nn.relu(x)
+
+    def grad(upstream):
+        dx = tf.where(x < 0., 0., 1.)
+        return tf.maximum(upstream * dx, 0)
+    return y, grad
+
+
+@tf.custom_gradient
+def guided_linear(x):
+    y = x
+
+    def grad(upstream):
+        return tf.maximum(upstream, 0)
+    return y, grad
